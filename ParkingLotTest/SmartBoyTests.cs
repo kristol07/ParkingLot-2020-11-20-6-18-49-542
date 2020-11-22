9381ce098a87cc60f54c65ce17cabefd8ad6fb6b
Lin Liu <LLiu41@slb.com>
2020-11-22 16:23:34 +0800

    Test: check smart boy's parking startegy

---
+++ ParkingLotTest/SmartBoyTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ParkingLot;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class SmartBoyTests
+    {
+        [Fact]
+        public void Should_park_new_car_only_to_lot_which_has_more_positions()
+        {
+            var boy = new SmartBoy();
+            var lot1 = new Lot("loca1", 2);
+            var lot2 = new Lot("loca2", 3);
+            boy.Lots = new[] { lot1, lot2 };
+
+            string message;
+            var locations = new List<string>();
+            foreach (var car in TestData.GetCars(4))
+            {
+                var ticket = boy.Park(car, out message);
+                locations.Add(ticket.GetLotLocation());
+                Assert.Empty(message);
+            }
+
+            Assert.Equal(lot2.GetLocation(), locations[0]);
+            Assert.Equal(lot2.GetLocation(), locations[1]);
+            Assert.Equal(lot1.GetLocation(), locations[2]);
+            Assert.Equal(lot2.GetLocation(), locations[3]);
+        }
+    }
+}
---
+++ ParkingLotTest/TestData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ParkingLot;
+
+namespace ParkingLotTest
+{
+    public static class TestData
+    {
+        public static IEnumerable<Car> GetCars(int number)
+        {
+            int count = 0;
+            while (count++ < number)
+            {
+                var random = new Random();
+                var letters = Enumerable.Range('A', 'Z' - 'A' + 1)
+                    .Select(c => ((char)c).ToString());
+                var numbers = Enumerable.Range(0, 10).Select(c => c.ToString());
+                var randomLicenseNumber = letters.Concat(numbers)
+                    .OrderBy(c => random.Next())
+                    .Take(4)
+                    .Aggregate((a, b) => $"{a}{b}");
+                yield return new Car(randomLicenseNumber);
+            }
+        }
+    }
+}
